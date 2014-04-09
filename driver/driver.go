a95fb970bc22f2b2b16495b79511f5bef7ccf5b6
btracey <bdtinternet@gmail.com>
2014-04-08 17:00:18 -0700

    Many cleanups to driver before v1.0

--- driver/driver.go
+++ driver/driver.go
@@ -11,16 +11,41 @@ import (
 	"path/filepath"
 	"reflect"
 	"strconv"
-	"sync"
 
 	"github.com/btracey/su2tools/config"
 )
 
-// SU2Syscall specifies the way to execute SU2 a given Driver
-type SU2Syscaller interface {
-	SyscallString(d *Driver) (string, []string) // Returns the exec name and the arguments to be called by exec.Command
-	Concurrently() bool                         // Given a list of drivers, should they be run concurrently or in serial
-	NumCores() int                              // How many cores does it want
+// Status is a type that represents if the computation specified by the driver
+// has been computed, and if not, why not. Note that this is not the status
+// during a call to Driver.Run, but rather used for checking if this config file
+// has already been successfully been completed
+type Status int
+
+func (s Status) String() string {
+	return statusMap[s]
+}
+
+const (
+	Computed Status = iota + 1
+	NoConfigFile
+	ErrorParsingConfig
+	UnequalOptions
+	NoSolutionFile
+)
+
+var statusMap = map[Status]string{
+	Computed:           "computed",
+	NoConfigFile:       "no existing config file",
+	ErrorParsingConfig: "error parsing the existing config file in the working directory",
+	UnequalOptions:     "options in the config file are not the same",
+	NoSolutionFile:     "no solution file found",
+}
+
+// Syscaller is a provides the system call to execute a Driver. The returned
+// arguments will be passed directly to exec.Cmd
+type Syscaller interface {
+	Syscall(d *Driver) (string, []string) // Returns the exec name and the arguments to be called by exec.Command
+	NumCores() int                        // How many cores does it want
 }
 
 // A FileWriter is a type that needs to print a file to disk before executing the driver
@@ -28,45 +53,39 @@ type FileWriter interface {
 	WriteFile(d *Driver) error
 }
 
-// Serial runs SU2 on one processor
-type Serial struct {
-	Concurrent bool
-}
+// Serial runs SU2 on one processor.
+type Serial struct{}
 
-func (s Serial) SyscallString(d *Driver) (execname string, args []string) {
+func (s Serial) Syscall(d *Driver) (execname string, args []string) {
 	return "SU2_CFD", []string{d.Config}
 }
 
-func (s Serial) Concurrently() bool {
-	return s.Concurrent
-}
 func (s Serial) NumCores() int {
 	return 1
 }
 
-// Parallel runs SU2 in parallel with the specified number of cores
+// Parallel runs SU2 in parallel with the specified number of cores. SU2 must
+// be compiled with MPI
 type Parallel struct {
-	NumCores   int
-	Concurrent bool
+	Cores int
 }
 
-func (p Parallel) SyscallString(d *Driver) (execname string, args []string) {
+func (p Parallel) Syscall(d *Driver) (execname string, args []string) {
 	execname = "parallel_computation.py"
-	args = []string{"-f", d.Config, "-p", strconv.Itoa(p.NumCores)}
+	args = []string{"-f", d.Config, "-p", strconv.Itoa(p.Cores)}
 	return
 }
 
-func (p Parallel) Concurrently() bool {
-	return p.Concurrent
+func (p Parallel) NumCores() int {
+	return p.Cores
 }
 
-// Cluster runs SU2 over a cluster. Not working.
-type Cluster struct {
-	Cores      int
-	Concurrent bool
+// Cluster runs SU2 via a call to Slurm. This does not work at present.
+type Slurm struct {
+	Cores int
 }
 
-func (c Cluster) SyscallString(d *Driver) (execname string, args []string) {
+func (c Slurm) Syscall(d *Driver) (execname string, args []string) {
 	fmt.Println("In cluster syscall")
 	//execname = "sbatch"
 	//args = []string{"slurm_script.run"}
@@ -98,79 +117,100 @@ func (c Cluster) WriteFile(d *Driver) error {
 }
 */
 
-func (c Cluster) Concurrently() bool {
-	return c.Concurrent
-}
-
-// Driver is a config type for running SU2
+// Driver specifies a case for running SU2.
 type Driver struct {
 	Name       string            // Identifier for the case
 	Options    *config.Options   // OptionList for the case
 	Config     string            // Name of the config filename (relative to working directory)
 	Wd         string            // Working directory of SU2
-	Stdout     string            // Where should the run output go (relative to working directory or StIO)
+	Stdout     string            // Where to redict Stdout (relative to working directory). Will be set to Stdout if ==""
+	Stderr     string            // Where to redirct Stderr (relative to working directory). Will be set to Stderr if ==""
 	OptionList config.OptionList // Which options to print to the config file
 	FancyName  string            // Longer name (can be used for plot legends or something)
 }
 
-// IsComputed checks if the case specified by the driver has already been run
+// IsComputed returns true if driver.Status() == Computed
 func (d *Driver) IsComputed() bool {
+	return d.Status() == Computed
+}
+
+// Status returns the status of the computation. If this particular set of options
+// has been computed before (defined by the config file and the working directory)
+// it returns Computed, otherwise it returns one of the other status constants.
+func (d *Driver) Status() Status {
 	// First, check if the file exists
-	f, err := os.Open(d.Fullpath(d.Config))
+	f, err := os.Open(d.fullpath(d.Config))
 	if err != nil {
-		fmt.Println("not computed, no config file")
-		return false
+		return NoConfigFile
 	}
 
 	// Next, check if the options file is the same
 	oldOptions, _, err := config.Read(f)
 	if err != nil {
-		fmt.Println("not computed, error reading config file ", err)
-		return false
+		return ErrorParsingConfig
 	}
 	if !reflect.DeepEqual(d.Options, oldOptions) {
-		fmt.Println("not computed, config files unequal")
-		return false
+		return UnequalOptions
 	}
-	// Same config file already exists, see if the solution file exists
-	_, err = os.Open(d.Fullpath(d.Options.SolutionFlowFilename))
+	// Same config file already exists and is the same, see if the solution file exists
+	_, err = os.Open(d.fullpath(d.Options.SolutionFlowFilename))
 	if err != nil {
-		fmt.Println("not computed, no solution file")
-		return false
+		return NoSolutionFile
 	}
-	fmt.Println("Is computed")
-	return true
+	// The cases are the same
+	return Computed
 }
 
-// Run executes SU2 with the given settings. The config file will be written to file,
-// and the appropriate arguments will be taken from the SU2Syscaller
-func (d *Driver) Run(su2call SU2Syscaller) error {
+// Run executes SU2 with the given Syscaller. Run writes the config file (as
+// specified by driver.Config and driver.Wd), uses the Syscaller to get
+// the arguments to exec.Command, and calls exec.Cmd.Run with the provided
+// working directory, stdout, and stderr
+func (d *Driver) Run(su2call Syscaller) error {
 
 	err := os.MkdirAll(d.Wd, 0700)
 	if err != nil {
 		return errors.New("driver: error creating working directory: " + err.Error())
 	}
 	// Write the config file
-	f, err := os.Create(d.Fullpath(d.Config))
+	f, err := os.Create(d.fullpath(d.Config))
 	if err != nil {
 		return err
 	}
 	d.Options.WriteConfig(f, d.OptionList)
 	f.Close()
 
-	// Open the standard out file
-	stdout, err := os.Create(d.Fullpath(d.Stdout))
-	defer stdout.Close()
-	if err != nil {
-		return errors.New("driver: error creating stdout: " + err.Error())
+	var stdout io.Writer
+
+	if d.Stdout == "" {
+		stdout = os.Stdout
+	} else {
+		// Open the standard out file
+		f, err := os.Create(d.fullpath(d.Stdout))
+		if err != nil {
+			return errors.New("driver: error creating stdout: " + err.Error())
+		}
+		defer f.Close()
+		stdout = f
+	}
+	var stderr io.Writer
+	if d.Stderr == "" {
+		stderr = os.Stderr
+	} else {
+		// Open the standard error file
+		f, err := os.Create(d.fullpath(d.Stderr))
+		if err != nil {
+			return errors.New("driver: error creating stderr: " + err.Error())
+		}
+		defer f.Close()
+		stderr = f
 	}
 
 	// Create the command
-	name, args := su2call.SyscallString(d)
+	name, args := su2call.Syscall(d)
 	cmd := exec.Command(name, args...)
 	cmd.Stdout = stdout
 	cmd.Dir = d.Wd
-	cmd.Stderr = os.Stdout
+	cmd.Stderr = stderr
 
 	fmt.Println("executing execname", "name = ", name, " args = ", args)
 	return cmd.Run()
@@ -178,13 +218,13 @@ func (d *Driver) Run(su2call SU2Syscaller) error {
 
 // CopyRestartToSolution copies the restart file to the solution file
 func (d *Driver) CopyRestartToSolution() error {
-	s, err := os.Create(d.Fullpath(d.Options.SolutionFlowFilename))
+	s, err := os.Create(d.fullpath(d.Options.SolutionFlowFilename))
 	if err != nil {
 		return fmt.Errorf("copyrestart: error opening SolutionFlowFilename: %v", err)
 	}
 	defer s.Close()
 
-	r, err := os.Open(d.Fullpath(d.Options.RestartFlowFilename))
+	r, err := os.Open(d.fullpath(d.Options.RestartFlowFilename))
 	if err != nil {
 		return fmt.Errorf("copyrestart: error opening RestartFlowFilename: %v", err)
 	}
@@ -194,10 +234,11 @@ func (d *Driver) CopyRestartToSolution() error {
 }
 
 // Fullpath returns the full path
-// TODO: Does this need to be public
-func (d *Driver) Fullpath(relpath string) string {
+func (d *Driver) fullpath(relpath string) string {
 	return filepath.Join(d.Wd, relpath)
 }
+
+/*
 
 // SetRelativeOptions sets the options structure relative to the from the config file in filename.
 // filename is the base config file
@@ -224,13 +265,14 @@ func (d *Driver) SetRelativeOptions(filename string, isrel bool, delta config.Fi
 	d.OptionList = optionList
 	return nil
 }
+*/
 
 func (d *Driver) fileFromFilename(filename string, isrel bool, create bool) (*os.File, error) {
 	if isrel {
 		if d.Wd == "" {
 			return nil, errors.New("driver: cannot use relative path; working directory not set")
 		}
-		filename = d.Fullpath(filename)
+		filename = d.fullpath(filename)
 	}
 
 	//fmt.Println("Filename = ", filename)
@@ -245,6 +287,7 @@ func (d *Driver) fileFromFilename(filename string, isrel bool, create bool) (*os
 	return os.Open(filename)
 }
 
+/*
 // RunCases runs a list of Drivers with the input SU2Syscaller if they have not been computed.
 // Depending on the settings of the Syscaller, the cases will either be
 // run concurrently or in parallel. The return is a list of errors, one for
@@ -306,3 +349,4 @@ func runcase(redo bool, driver *Driver, su2call SU2Syscaller) error {
 	}
 	return err
 }
+*/
